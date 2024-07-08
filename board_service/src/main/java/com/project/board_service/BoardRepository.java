468e61192ff3162963a235966b9ed829b6ebaca0
bongbong2 <106155992+bongbongbon@users.noreply.github.com>
2024-07-08 21:22:49 +0900

    123
    1

--- board_service/src/main/java/com/project/board_service/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.board_service;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface BoardRepository extends JpaRepository<Board,Long> {
-    boolean existsByBoardName(String boardName);
-    void deleteByBoardID(Long boardID);}
--- board_service/src/main/java/com/project/board_service/DuplicationNameException.java
+++
@@ -1,3 +0,0 @@
-package com.project.board_service;
-public class DuplicationNameException extends RuntimeException{
-    public DuplicationNameException(String message){super(message);}}
