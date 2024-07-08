8b4b37db59534479db47171c4853950cdf9b4504
mtgv999 <petermgcz@gmail.com>
2024-07-08 20:45:48 +0900

    Commit - 게시판 CRUD 작성2

--- board_service/src/main/java/com/project/board_service/BaseEntity.java
+++ board_service/src/main/java/com/project/board_service/BaseEntity.java
@@ -1,7 +1,12 @@
 package com.project.board_service;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
-@Getter
+@Getter @MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
 public class BaseEntity {
     private LocalDateTime createdTime=LocalDateTime.now();
     private LocalDateTime updateTime=LocalDateTime.now();}
