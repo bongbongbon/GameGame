61a472588ac29581591491a0b0eb63551eebc7da
bongbong2 <106155992+bongbongbon@users.noreply.github.com>
2024-07-23 15:35:08 +0900

    1

--- src/main/java/com/project/gamegame/quiz/service/ResultService.java
+++ src/main/java/com/project/gamegame/quiz/service/ResultService.java
@@ -1,9 +1,11 @@
 package com.project.gamegame.quiz.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ResultService {
-    
+
 
 }
